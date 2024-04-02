c8854b071d31db6e58e055fe42a5f2394ec9bebf
mKamranIqbal <128483787+mKamranIqbal@users.noreply.github.com>
2024-04-02 15:14:52 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,33 @@
+let score  = 33
+let score1  = "33"
+let score2 = "33abc"
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score1)
+let valueInNumber2 = Number(score2)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber2);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "kamran"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "Kamran"=> true;
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
