58fcea4ddb7eef8ccab3dba2291b41e906b9473a
mKamranIqbal <128483787+mKamranIqbal@users.noreply.github.com>
2024-04-03 16:27:13 +0000

    strings practice.

---
+++ 01_basics/05_string.js
@@ -0,0 +1,18 @@
+
+const name = "kamran"
+
+const firstName = "hassan"
+
+const paragraph = "I have many friends but Ommo is my best friend."
+
+let nickName = "ommo";
+
+// console.log(name);
+// console.log(name.charAt(2));
+// console.log(name.indexOf('r'));
+
+// console.log(firstName.toUpperCase());
+// console.log(firstName.includes('ass'));
+// console.log(firstName.length)
+// console.log(paragraph.toLowerCase().lastIndexOf(nickName));
+console.log(paragraph.normalize("NFC"));
