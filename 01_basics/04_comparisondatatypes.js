32f7c2dc3e42b501df3f54abc3d86bbb1e01552d
mKamranIqbal <128483787+mKamranIqbal@users.noreply.github.com>
2024-04-03 05:23:47 +0000

    Comparison of Datatypes

---
+++ 01_basics/04_comparisondatatypes.js
@@ -0,0 +1,27 @@
+
+// console.log(2 > 1)
+
+// console.log(2 < 1)
+
+// console.log(2 == 1)
+
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+console.log("2" > 1) // true
+console.log("2" == 2) // ture in this case datatype is not check that's y result is true
+
+// Strick Check
+console.log("2" === 2) // false  in this case with data comparison operator also check datatype.
