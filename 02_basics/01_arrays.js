cc001eba52952880fb0b3b7bdee6b5614e93ccb8
mKamranIqbal <128483787+mKamranIqbal@users.noreply.github.com>
2024-04-04 12:54:01 +0000

    basic arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,46 @@
+ //  arrays.
+
+ const myFirstArray = [0,1,2,3,4,5,6,7,8];
+ const myHeros = ['maria', 'marvelous', 'superman', 'spiderman'];
+
+ const arr2 = new Array(1, 2,3 ,4);
+
+//  console.log(myFirstArray[2]);
+
+ // Array Methods
+
+
+//  arr2.push(5)
+// arr2.push(6)
+// arr2.push(7)
+//  arr2.pop()
+
+// arr2.unshift(9)
+
+arr2.shift()
+
+// console.log(arr2)
+// console.log(arr2.includes(9))
+// console.log(arr2.indexOf(9))
+
+
+// const newArr = arr2.join();
+// console.log(arr2);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// const newArr = new Array(1,2,3,4,)
+
+// const mySlice = newArr.slice(1,2)
+// console.log("A", newArr)
+// console.log(mySlice)
+
+// const mySplice = newArr.splice(1,3)
+// console.log("B", newArr)
+// console.log(mySplice)
+
+
+
+
+
+
