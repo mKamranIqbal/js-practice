2b74d05a4d6da1082b4e3658a9ddfd70a00b719c
mKamranIqbal <128483787+mKamranIqbal@users.noreply.github.com>
2024-04-24 21:16:43 +0000

    arrows function IIFE

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,15 @@
+(function Db(){
+    console.log("DB connected");
+
+})();
+
+// Db();
+
+(chai = (name) => {
+    console.log(`db connection 2 ${name}`)
+})('Kamran');
+
+
+((marks)=>{
+    console.log(`I have done my all work with perfection and I got ${marks} marks.`)
+})(580)
